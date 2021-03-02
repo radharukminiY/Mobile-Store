2f0397db7ad3b92f69880c91a741fb6172ab2d39
radharukminiY <rukku558@gmail.com>
2021-03-02 22:38:40 +0530

    commit 9

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import {Switch,Route,Fragment} from 'react-router-dom';
+import React ,{Fragment}from 'react';
+import {Switch,Route,} from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/home';
 import ProductList from './components/productlist';
 import Details from './components/details';
-import Cart from './components/cart';
+import Cart from './components/Cart/Cart';
 import Default from './components/Default';
-import Modal from './components/modal';
+import Modal from './components/model';
 
 function App() {
   return(
     <Fragment>
     <Navbar />
     <Switch>
-    <Route exact path ="/" component ={ProductList}</Route>
-    <Route exact path ="/details" component ={Details}</Route>
-    <Route exact path ="/cart" component ={Cart}</Route>
-    <Route exact component ={Default}</Route>
+    <Route exact path ="/" component ={ProductList}></Route>
+    <Route exact path ="/details" component ={Details}></Route>
+    <Route exact path ="/cart" component ={Cart}></Route>
+    <Route exact component ={Default}></Route>
     </Switch>
-    <Model />
+    <Modal/>
     </Fragment>
   );
 }
